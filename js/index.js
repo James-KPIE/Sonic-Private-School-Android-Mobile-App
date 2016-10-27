642ef38cb685b06bfa67580fdb427e71372841e3
James-KPIE <fxroyalmoneysolution@gmail.com>
2016-10-27 03:43:11 +0100

    We jus made significant improvement and hoping the push notification system works Signed-off-by: James-KPIE <fxroyalmoneysolution@gmail.com>

--- js/index.js
+++ js/index.js
@@ -47,3 +47,24 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+
+// Add to index.js or the first page that loads with your app.
+// For Intel XDK and please add this to your app.js.
+
+document.addEventListener('deviceready', function () {
+  // Enable to debug issues.
+  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+  
+  var notificationOpenedCallback = function(jsonData) {
+    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+  };
+
+  window.plugins.OneSignal
+    .startInit("2a291ea2-4109-4cf7-8308-46d05374d40e", "680179313788")
+    .handleNotificationOpened(notificationOpenedCallback)
+    .endInit();
+  
+  // Sync hashed email if you have a login system or collect it.
+  //   Will be used to reach the user at the most optimal time of day.
+  // window.plugins.OneSignal.syncHashedEmail(userEmail);
+}, false);
